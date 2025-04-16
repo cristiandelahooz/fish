92332bcd82eec3bf4492c2d702b2aab74e04acbd
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-16 13:10:58 -0400

    feat(conf.d): add environment variables

---
+++ conf.d/env.fish
@@ -0,0 +1,9 @@
+set -gx PATH $HOME/.scripts $PATH
+set -gx FZF_DEFAULT_OPTS '
+  --color=fg:#ecb680,fg+:#d0d0d0,bg:#000000,bg+:#000000
+  --color=hl:#5b8dbf,hl+:#5fd7ff,info:#afaf87,marker:#87ff00
+  --color=prompt:#d7005f,spinner:#af5fff,pointer:#af5fff,header:#87afaf
+  --color=border:#262626,label:#aeaeae,query:#d9d9d9
+  --border="sharp" --border-label="Wornux" --border-label-pos="19" --preview-window="border-thinblock"
+  --prompt=">" --marker="✸" --pointer="⛩️" --separator="─"
+  --scrollbar="│" --layout="reverse"'
