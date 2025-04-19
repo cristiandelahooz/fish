e5d55799f6c5b19badadd5e82193c27fe8462df4
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-19 12:58:21 -0400

    feat(conf.d): add init configuration file

---
+++ conf.d/init.fish
@@ -0,0 +1,16 @@
+command -qv nvim && alias vim nvim
+command -qv zoxide && zoxide init fish | source
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
