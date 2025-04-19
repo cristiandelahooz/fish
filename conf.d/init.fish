503cc7a9a01400705dec9c1526cac575cfc4135b
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-19 13:00:24 -0400

    fix(init): fix OS config files path

--- conf.d/init.fish
+++ conf.d/init.fish
@@ -3,14 +3,14 @@ command -qv zoxide && zoxide init fish | source
 
 switch (uname)
     case Darwin
-        source (dirname (status --current-filename))/config-osx.fish
+        source (dirname (status --current-filename))/../config-osx.fish
     case Linux
-        source (dirname (status --current-filename))/config-linux.fish
+        source (dirname (status --current-filename))/../config-linux.fish
     case '*'
-        source (dirname (status --current-filename))/config-windows.fish
+        source (dirname (status --current-filename))/../config-windows.fish
 end
 
-set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+set LOCAL_CONFIG (dirname (status --current-filename))/../config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
