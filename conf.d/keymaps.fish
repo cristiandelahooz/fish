bfb079b034cd208c45b339d04a3aaa0cd8f2682e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-23 21:23:35 -0400

    refactor: move missing keymaps in fish_user_key_bindings.fish to conf.d/keymaps

--- conf.d/keymaps.fish
+++ conf.d/keymaps.fish
@@ -1,3 +1,9 @@
 bind \cL forward-char
 bind \cg ide
 bind \ct tmux-sessionizer
+bind \cf fzf_change_directory
+bind \cl forward-char
+bind \cd delete-char
+
+# fzf plugin
+fzf_configure_bindings --directory=\co
